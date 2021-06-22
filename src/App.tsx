beaf8349a0a6394bfd65cbeb65ce995955142289
wangpengfei <2601339907@qq.com>
2021-06-22 09:17:47 +0800

    feat: 增加开始暂停键盘控制

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import MainScreen from "./components/mainScreen";
 import About from "./components/about";
 import RealtimeInfo from "./components/realtimeInfo";
@@ -8,6 +8,33 @@ import "./app.less";
 
 const Tetris = ():JSX.Element => {
     const [status, setStatus] = useState<GameStatus>(GameStatus.UNSTART);
+    const [time, setTime] = useState<number>(0); // 时间
+    const [score, setScore] = useState<number>(0); // 分数
+
+    // 监听暂停或者开始事件
+    useEffect(() => {
+        const listener = (event: KeyboardEvent) => {
+            if (event.key === " ") {
+                setStatus(status => {
+                    switch (status) {
+                        case GameStatus.UNSTART:
+                        case GameStatus.OVER:
+                        case GameStatus.PAUSE:
+                            return GameStatus.RUNNING;
+                        case GameStatus.RUNNING:
+                            return GameStatus.PAUSE;
+                        default:
+                            throw new Error(`unimplement game status: ${status}.`);
+                    }
+                });
+            }
+        };
+        window.addEventListener("keydown", listener);
+
+        return () => {
+            window.removeEventListener("keydown", listener);
+        }
+    }, []);
 
     return (
         <div className="tetris">
